e5abb2599b5a07dcffda9bc5a94bcb507fa80a3d
AVMSTEVE <mstevens@svb.nl>
2025-11-20 15:13:18 +0100

    rules drl

--- src/main/java/io/matthijs/FindApprovedLoansEndpoint.java
+++ src/main/java/io/matthijs/FindApprovedLoansEndpoint.java
@@ -1,36 +1,58 @@
-//package io.matthijs;
-//
-//import jakarta.inject.Inject;
-//import jakarta.ws.rs.Consumes;
-//import jakarta.ws.rs.POST;
-//import jakarta.ws.rs.Path;
-//import jakarta.ws.rs.Produces;
-//import jakarta.ws.rs.core.MediaType;
-//import org.kie.api.runtime.KieRuntimeBuilder;
-//import org.kie.api.runtime.KieSession;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Path("/find-approved")
-//public class FindApprovedLoansEndpoint {
-//
+package io.matthijs;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieRuntimeBuilder;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Path("/find-approved")
+public class FindApprovedLoansEndpoint {
+
 //    @Inject
 //    KieRuntimeBuilder kieRuntimeBuilder;
-//
-//    @POST()
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public List<LoanApplication> executeQuery(LoanAppDto loanAppDto) {
+
+    @POST()
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public List<LoanApplication> executeQuery(LoanAppDto loanAppDto) {
+
+        KieServices kieServices = KieServices.Factory.get();
+        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+        List<String> rules = Arrays.asList("rules1.drl");
+        for (String rule : rules) {
+            kieFileSystem.write(ResourceFactory.newClassPathResource(rule));
+        }
+        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+        kieBuilder.buildAll();
+        KieRepository kieRepository = kieServices.getRepository();
+        ReleaseId krDefaultReleaseId = kieRepository.getDefaultReleaseId();
+        KieContainer kieContainer = kieServices.newKieContainer(krDefaultReleaseId);
+        KieSession session = kieContainer.newKieSession();
+
 //        KieSession session = kieRuntimeBuilder.newKieSession();
-//        List<LoanApplication> approvedApplications = new ArrayList<>();
-//
-//        session.setGlobal("approvedApplications", approvedApplications);
-//        session.setGlobal("maxAmount", loanAppDto.getMaxAmount());
-//        loanAppDto.getLoanApplications().forEach(session::insert);
-//
-//        session.fireAllRules();
-//        session.dispose();
-//        return approvedApplications;
-//    }
-//}
+        List<LoanApplication> approvedApplications = new ArrayList<>();
+
+        session.setGlobal("approvedApplications", approvedApplications);
+        session.setGlobal("maxAmount", loanAppDto.getMaxAmount());
+        loanAppDto.getLoanApplications().forEach(session::insert);
+
+        session.fireAllRules();
+        session.dispose();
+        return approvedApplications;
+    }
+}
